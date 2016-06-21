dd4287a383056f8a4a43e4c558f44709f23ddb42
Jakob Knutsen <jakobknutsen@gmail.com>
2016-06-21 23:32:57 +0200

    Add movement and change of text

--- AdDimensionAdvertiser/Assets/Scripts/MoveTowardsTarget.cs
+++ AdDimensionAdvertiser/Assets/Scripts/MoveTowardsTarget.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class MoveTowardsTarget : MonoBehaviour {
 
-	// Use this for initialization
+	Vector3 defaultPosition = new Vector3(-3.0f, 1.40f, 35f);
+	string[] slogans = new string[5] {"Hackathon", "24 hours", "Axel Springer", "HACKDAY!", "Sign up now!"};
+	int i = 0;
+
 	void Start () {
-	
+		GetComponent<TextMesh>().text = slogans[i++];
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (transform.position.z > -10) {
+			transform.Translate(Vector3.back * Time.deltaTime * 35);
+		} else {
+			transform.position = defaultPosition;
+			GetComponent<TextMesh>().text = slogans[i++];
+		}
 	}
 }
