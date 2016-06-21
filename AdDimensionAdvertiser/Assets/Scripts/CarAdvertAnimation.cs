9bf3a660b0eefdc8147926bef8a5a440236d6237
MCarmen Jimenez Campos <mcarmen.jimenez.campos@gmail.com>
2016-06-21 21:58:02 +0200

    rotating car

---
+++ AdDimensionAdvertiser/Assets/Scripts/CarAdvertAnimation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAdvertAnimation : MonoBehaviour {
+	
+	void Update () {
+		transform.RotateAround (new Vector3 (0, 0, 0), new Vector3 (0, 1, 0), 30f * Time.deltaTime);
+	}
+}
