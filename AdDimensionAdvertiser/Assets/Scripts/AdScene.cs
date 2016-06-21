d20f013ec7936158d25037d7d8b60c82a51c3d40
Ryan Hendry <ryan.hendry@affiliatewindow.com>
2016-06-21 22:11:51 +0200

    scene script

---
+++ AdDimensionAdvertiser/Assets/Scripts/AdScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdScene : MonoBehaviour {
+
+	private float _totalRun= 1.0f;
+	private float _lockTime = 5.0f;
+	private float _runningTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("STARTED");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("UPDATED");
+		_runningTime += Time.deltaTime * 1;
+
+		if( _runningTime >= _lockTime)
+		{
+			_runningTime = 0f;
+			string originScene = PlayerPrefs.GetString ("originScene"); 
+			Debug.Log ("!!!!" + originScene);
+			PlayerPrefs.DeleteKey ("originScene");
+			Application.LoadLevel (originScene);
+		}
+	
+	}
+}
