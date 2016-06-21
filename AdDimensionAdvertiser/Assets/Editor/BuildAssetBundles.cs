2ba3cf6b2ad70b963aa5f5a071d645faf13dc368
Ryan Hendry <ryan.hendry@affiliatewindow.com>
2016-06-21 20:23:22 +0200

    Adding buildAssetBundles script

---
+++ AdDimensionAdvertiser/Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles
+{
+	[MenuItem ("Build/BuildWebplayerStreamed")]
+	public static void MyBuild(){
+		string[] levels = new string[] {"Assets/Scenes/advert.unity"};
+		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Assets/Build/advert.unity3d", BuildTarget.StandaloneLinuxUniversal); 
+	}
+}
