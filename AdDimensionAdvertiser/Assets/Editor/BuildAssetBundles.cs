5e5445e53ed7cbcfe13f094f5b31bd653b141234
Jakob Knutsen <jakobknutsen@gmail.com>
2016-06-22 03:06:39 +0200

    Set build settings for Android

--- AdDimensionAdvertiser/Assets/Editor/BuildAssetBundles.cs
+++ AdDimensionAdvertiser/Assets/Editor/BuildAssetBundles.cs
@@ -4,7 +4,7 @@ public class CreateAssetBundles
 {
 	[MenuItem ("Build/BuildWebplayerStreamed")]
 	public static void MyBuild(){
-		string[] levels = new string[] {"Assets/Scenes/advert.unity"};
-		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Assets/Build/advert.unity3d", BuildTarget.StandaloneLinuxUniversal); 
+		string[] levels = new string[] {"Assets/Scenes/AxelSpringerHackday.unity"};
+		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Assets/Build/advert.unity3d", BuildTarget.Android); 
 	}
 }
