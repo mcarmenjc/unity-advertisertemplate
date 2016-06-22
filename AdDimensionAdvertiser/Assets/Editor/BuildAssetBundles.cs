e6af6a692650c7bbc30421045755cf2d5909b044
Jakob Knutsen <jakobknutsen@gmail.com>
2016-06-22 04:17:09 +0200

    Add exit script to CocaCola scene

--- AdDimensionAdvertiser/Assets/Editor/BuildAssetBundles.cs
+++ AdDimensionAdvertiser/Assets/Editor/BuildAssetBundles.cs
@@ -4,7 +4,7 @@ public class CreateAssetBundles
 {
 	[MenuItem ("Build/BuildWebplayerStreamed")]
 	public static void MyBuild(){
-		string[] levels = new string[] {"Assets/Scenes/AxelSpringerHackday.unity"};
+		string[] levels = new string[] {"Assets/Scenes/AxelSpringerHackday.unity","Assets/Scenes/CarAdvert.unity","Assets/Scenes/CocaCola.unity"};
 		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Assets/Build/advert.unity3d", BuildTarget.Android); 
 	}
 }
